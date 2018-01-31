058f7e09884b28b0d347fab1cc50dc369f1c2be9
TrentonSarnowski <sarnowskit@msoe.edu>
2018-01-31 09:13:07 -0600

    altered the config file to be a static class

--- polymorphs-frontend-master/src/config.js
+++ polymorphs-frontend-master/src/config.js
@@ -1,4 +1,4 @@
-const configuration = {
-address: "127.0.0.1:8080/api/gene_search",
-    end: null
+export default class configuration {
+    static address = "127.0.0.1:8103"
+    static end = null
 }
